6a60671fc0d5a0b359435b3f45e81a64c242da67
HARSHA  BUDDANA <65592890+hbuddana@users.noreply.github.com>
2023-09-09 14:09:02 -0500

    Update 52_BST_Constructor.py

--- Section_13_Tree/52_BST_Constructor.py
+++ Section_13_Tree/52_BST_Constructor.py
@@ -1,21 +1,20 @@
+# Define a Node class to represent individual nodes in the binary search tree.
 class Node:
     def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
-        
+        self.value = value  # The value stored in this node.
+        self.left = None    # Reference to the left child node.
+        self.right = None   # Reference to the right child node.
 
+# Define a BinarySearchTree class to manage the binary search tree.
 class BinarySearchTree:
     def __init__(self):
-        self.root = None
+        self.root = None  # Initialize the root of the tree as None.
 
-
-
-
+# Create an instance of the BinarySearchTree class.
 my_tree = BinarySearchTree()
 
+# Print the root of the tree (initially None).
 print(my_tree.root)
-
 
  
 """
