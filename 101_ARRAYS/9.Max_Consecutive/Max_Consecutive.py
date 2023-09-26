31c3f47f4f135a90bd3063f8444b3fc8618d32d3
HARSHA  BUDDANA <65592890+hbuddana@users.noreply.github.com>
2023-09-26 09:33:09 -0500

    Add files via upload

---
+++ 101_ARRAYS/9.Max_Consecutive/Max_Consecutive.py
@@ -0,0 +1,19 @@
+
+arr = [1,1,0,1,1,1,1,1]
+
+maxi = 0
+count = 0 
+
+for i in range(len(arr)):
+    if arr[i] == 1:
+        count += 1
+        
+    else:
+        count = 0
+
+    maxi = max(count,maxi)
+    
+print(maxi)
+
+
+
